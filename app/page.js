56f7a7e714170e8a04404dafc588ee08caa25f3b
MohamedGlalAbdelazeem <mohamed539@gmail.com>
2024-09-08 08:07:38 +0300

    .

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Hero from "./_components/Hero";
-import ChooseUs from "./_components/ChooseUs"
-import OurCourses from "./_components/OurCourses"
+import ChooseUs from "./_components/ChooseUs";
+import OurCourses from "./_components/OurCourses";
+import Quizzes from "./_components/Quizzes"
 export default function Home() {
   return (
    <>
      <Hero/>
      <ChooseUs/>
      <OurCourses/>
+     <Quizzes/>
    </> 
   );
 }

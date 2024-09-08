f6674326411110f837e3ea5ab6d0081145d62d64
MohamedGlalAbdelazeem <mohamed539@gmail.com>
2024-09-08 09:57:32 +0300

    .

--- app/(auth)/sign-up/page.jsx
+++ app/(auth)/sign-up/page.jsx
@@ -2,161 +2,70 @@ import React from 'react';
 
 function Page() {
   return (
-    <section className="bg-white">
-      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-        <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
-          <img
-            alt="Background"
-            src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            className="absolute inset-0 h-full w-full object-cover opacity-80"
-          />
-          <div className="hidden lg:relative lg:block lg:p-12">
-            <a className="block text-white" href="#">
-              <span className="sr-only">Home</span>
-            </a>
-            <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-              Welcome to Squid 🦑
-            </h2>
-            <p className="mt-4 leading-relaxed text-white/90">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-              quibusdam aperiam voluptatum.
-            </p>
-          </div>
-        </section>
-
-        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
-          <div className="max-w-xl lg:max-w-3xl">
-            <div className="relative -mt-16 block lg:hidden">
-              <a
-                className="inline-flex items-center justify-center rounded-full bg-white text-blue-600 sm:size-20"
-                href="#"
-              >
-                <span className="sr-only">Home</span>
-              </a>
-              <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-                Welcome to Squid 🦑
-              </h1>
-              <p className="mt-4 leading-relaxed text-gray-500">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-                quibusdam aperiam voluptatum.
-              </p>
-            </div>
-
-            <form action="#" className="mt-8 grid grid-cols-6 gap-6">
-              <div className="col-span-6">
-                <label htmlFor="fullName" className="block text-sm font-medium text-gray-700">
-                  Full Name *
-                </label>
-                <input
-                  type="text"
-                  id="fullName"
-                  placeholder="Ex: Hisham Hassan"
-                  name="fullName"
-                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-                />
-              </div>
-
-              <div className="col-span-6 sm:col-span-3">
-                <label htmlFor="jobTitle" className="block text-sm font-medium text-gray-700">
-                  Job Title
-                </label>
-                <input
-                  type="text"
-                  id="jobTitle"
-                  name="jobTitle"
-                  placeholder="Ex: Instructor"
-                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-                />
-              </div>
-
-              <div className="col-span-6 sm:col-span-3">
-                <label htmlFor="phoneNumber" className="block text-sm font-medium text-gray-700">
-                  Phone Number *
-                </label>
-                <input
-                  type="text"
-                  id="phoneNumber"
-                  name="phoneNumber"
-                  placeholder="Ex: 010 15 800 24 8"
-                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-                />
-              </div>
-
-              <div className="col-span-6">
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                  Email *
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  placeholder="Your Email here"
-                  name="email"
-                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-                />
-              </div>
-
-              <div className="col-span-6 sm:col-span-3">
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                  Password *
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-                />
-              </div>
-
-              <div className="col-span-6 sm:col-span-3">
-                <label htmlFor="passwordConfirmation" className="block text-sm font-medium text-gray-700">
-                  Confirm Password *
-                </label>
-                <input
-                  type="password"
-                  id="passwordConfirmation"
-                  name="passwordConfirmation"
-                  className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
-                />
-              </div>
-
-              <div className="col-span-6">
-                <label htmlFor="marketingAccept" className="flex gap-4 items-center">
-                  <input
-                    type="checkbox"
-                    id="marketingAccept"
-                    name="marketingAccept"
-                    className="rounded-md border-gray-200 bg-white shadow-sm"
-                  />
-                  <span className="text-sm text-gray-700">
-                    By creating an account on MedLearn Hub, you agree to our{' '}
-                    <span className="border-b-2 border-black font-bold">Privacy Policy</span>. Please read these terms carefully.
-                  </span>
-                </label>
-              </div>
-
-              <div className="col-span-6">
-                <div className="flex flex-col items-center">
-                  <button
-                    type="submit"
-                    className="inline-block rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
-                  >
-                    Create an account
-                  </button>
-                  <div className="mt-4 text-center">
-                    <p className="text-sm text-gray-500">
-                      Do you have an account?{' '}
-                      <a href="#" className="text-gray-700 mx-1 font-bold text-lg">
-                        Sign in
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </form>
-          </div>
-        </main>
+    <div className="flex flex-col justify-center items-center font-[sans-serif] bg-gradient-to-r from-blue-800 to-blue-500 lg:h-screen p-6">
+    <div className="grid md:grid-cols-2 items-center gap-y-8 bg-white max-w-7xl w-full shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md overflow-hidden">
+      <div className="max-md:order-1 flex flex-col justify-center sm:p-8 p-4 bg-gradient-to-r from-blue-600 to-blue-700 w-full h-full">
+       image
       </div>
-    </section>
+
+      <form className="sm:p-8 p-4 w-full">
+        <div className="mb-12">
+          <h3 className="text-blue-500 text-3xl font-extrabold max-md:text-center">Register</h3>
+        </div>
+
+        <div className="grid lg:grid-cols-2 gap-6">
+          <div>
+            <label className="text-gray-800 text-sm mb-2 block">First Name</label>
+            <input name="name" type="text" className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500" placeholder="Enter name" />
+          </div>
+          <div>
+            <label className="text-gray-800 text-sm mb-2 block">Last Name</label>
+            <input name="lname" type="text" className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500" placeholder="Enter last name" />
+          </div>
+          <div>
+            <label className="text-gray-800 text-sm mb-2 block">Email Id</label>
+            <input name="email" type="text" className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500" placeholder="Enter email" />
+          </div>
+          <div>
+            <label className="text-gray-800 text-sm mb-2 block">Mobile No.</label>
+            <input name="number" type="number" className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500" placeholder="Enter mobile number" />
+          </div>
+          <div>
+            <label class="text-gray-800 text-sm mb-2 block">Password</label>
+              <div class="relative flex items-center">
+                <input name="password" type="password" required class="bg-transparent border border-gray-400 w-full text-gray-800 text-sm pl-4 pr-10 py-3 rounded-xl outline-blue-600" placeholder="Enter password" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-4 h-4 absolute right-4 cursor-pointer" viewBox="0 0 128 128">
+                  <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
+                </svg>
+              </div>
+            </div>
+            
+            <div>
+            <label class="text-gray-800 text-sm mb-2 block">Password</label>
+              <div class="relative flex items-center">
+                <input name="password" type="password" required class="bg-transparent border border-gray-400 w-full text-gray-800 text-sm pl-4 pr-10 py-3 rounded-xl outline-blue-600" placeholder="Enter password" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-4 h-4 absolute right-4 cursor-pointer" viewBox="0 0 128 128">
+                  <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
+                </svg>
+              </div>
+            </div>
+        </div>
+
+        <div className="flex items-center mt-6">
+          <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 shrink-0 rounded" />
+          <label for="remember-me" className="ml-3 block text-sm">
+          By creating an account on MedLearn Hub, you agree to our Privacy Policy . Please read these terms carefully.
+          </label>
+        </div>
+
+        <div className="mt-6 ">
+          <button type="button" className="w-full py-3 px-6 text-sm tracking-wide font-semibold rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none transition-all">
+            Sign up
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
   );
 }
 

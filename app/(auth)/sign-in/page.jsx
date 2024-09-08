f4f16143c4fa2e10f781dc9f33bda5d31b50a2bf
MohamedGlalAbdelazeem <mohamed539@gmail.com>
2024-09-08 12:38:59 +0300

    .

--- app/(auth)/sign-in/page.jsx
+++ app/(auth)/sign-in/page.jsx
@@ -8,31 +8,12 @@ function Page() {
        image
       </div>
 
-      <form className="sm:p-8 -mt-10  w-full">
+      <form className="sm:p-8 my-16 w-full">
         <div className="text-center my-10 w-full mx-auto">
            <span>Welcome to</span>
           <h3 className="text-blue-500 text-2xl font-extrabold max-md:text-center">MedLearn Hub</h3>
         </div>
-      {/* fullname */}
-        <div className="grid lg:grid-cols-1 my-6">
-          <div>
-            <label className="text-gray-800 text-sm mb-2 block">Full Name *</label>
-            <input name="name" type="text" className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500" placeholder="Ex : Hisham Hassan " />
-          </div>
-        </div>
-      {/* fullname */}
-      
-      <div className="grid lg:grid-cols-2 gap-6">
-        <div>
-            <label className="text-gray-800 text-sm mb-2 block">Job Title</label>
-            <input name="name" type="text" className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500" placeholder="Ex : Instructor " />
-          </div>
-          <div>
-            <label className="text-gray-800 text-sm mb-2 block">Phone Number *</label>
-            <input name="lname" type="text" className="bg-gray-100 w-full text-gray-800 text-sm px-4 py-3 rounded-md outline-blue-500" placeholder="Ex : 010 15 800 24 8" />
-          </div>
-        </div>
-
+     
        {/* email */}
        <div className="grid lg:grid-cols-1 my-6">
           <div>
@@ -43,18 +24,9 @@ function Page() {
       {/* email */}
 
          {/* password */}
-        <div className="grid lg:grid-cols-2 gap-6">
+        <div className="grid lg:grid-cols-1 gap-6">
            <div>
             <label class="text-gray-800 text-sm mb-2 block">Password *</label>
-              <div class="relative flex items-center">
-                <input name="password" type="password" required class="bg-transparent border border-gray-400 w-full text-gray-800 text-sm pl-4 pr-10 py-3 rounded-xl outline-blue-600" placeholder="***************" />
-                <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-4 h-4 absolute right-4 cursor-pointer" viewBox="0 0 128 128">
-                  <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
-                </svg>
-              </div>
-            </div>
-            <div>
-            <label class="text-gray-800 text-sm mb-2 block">Confirm Password *</label>
               <div class="relative flex items-center">
                 <input name="password" type="password" required class="bg-transparent border border-gray-400 w-full text-gray-800 text-sm pl-4 pr-10 py-3 rounded-xl outline-blue-600" placeholder="***************" />
                 <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-4 h-4 absolute right-4 cursor-pointer" viewBox="0 0 128 128">
@@ -66,18 +38,15 @@ function Page() {
        {/* password */}
 
        <div className="flex items-center mt-6">
-          <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 shrink-0 rounded" />
-          <label for="remember-me" className="ml-3 block text-sm">
-          By creating an account on MedLearn Hub, you agree to our Privacy Policy . Please read these terms carefully.
-          </label>
+           <p>Forget Password?</p>
         </div>
 
         <div className="mt-6 ">
           <button type="button" className="w-full py-3 px-6 text-sm tracking-wide font-semibold rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none transition-all">
-            Sign up
+          Sign In
           </button>
         <div className='w-full flex justify-center my-6'>
-            <span className=''>Do You have an account?  <button className='font-bold border-b-2 border-black' >Sign in</button></span>
+            <span className=''>Don’t have an accoun <button className='font-bold border-b-2 border-black' >Join Now</button></span>
         </div>
         </div>
       </form>

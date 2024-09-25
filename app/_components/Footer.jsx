ef6ccad5c37cbb43aa032162e7ac12ab2b84c6dd
MohamedGlalAbdelazeem <mohamed539@gmail.com>
2024-09-25 12:33:02 +0300

    .

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
-import { CiFacebook } from "react-icons/ci";
-import { FaTelegramPlane } from "react-icons/fa";
+import Image from "next/image";
 
 function Footer() {
     return (
@@ -32,11 +31,11 @@ function Footer() {
               MedLearn Hub is an online platform offering medical courses, tests, a blog, and a collection of medical books. The platform is designed for medical professionals and students, providing them with valuable learning resources and tools to enhance their knowledge. This documentation serves as a comprehensive guide for developers and clients, outlining the core features and functionalities of the platform.
             </p>
             <div className="flex flex-wrap gap-2">
-              <a href="#" className="mx-2 bg-bgFontColor text-white p-2 rounded-sm text-[16px] ">Join Our Community</a>
+              <a href="#" className="mx-2 bg-bgFontColor text-white p-2 rounded-lg text-[16px] ">Join Our Community</a>
               <a href="#" className="mx-2 border-2 border-white text-white p-2 rounded-lg">Browse Courses</a>
             </div>
           </div>
-          <div className="p-5 sm:w-3/12">
+          <div className="p-5 sm:w-3/12 mt-20">
             <ul>
               <li className="my-2">
                 <a className="text-white" href="#">About Us</a>
@@ -44,9 +43,31 @@ function Footer() {
               <li className="my-2">
                 <Link href="/pages/Privacy-Policy" className="text-white">Privacy Policy</Link>
               </li>
-              <li className="flex gap-4">
-               <CiFacebook className="text-white text-2xl bg-blue-500 rounded-full" />
-               <FaTelegramPlane className="text-white text-xl bg-blue-500 rounded-full" />
+              <li className="flex gap-4 mt-6">
+              <Image
+              src="/facebook.svg"
+              alt="facebook image" 
+              width={"20"}
+              height={"20"}
+             /> 
+                <Image
+              src="/Instagram_logo_2016.svg.webp"
+              alt="facebook image" 
+              width={"20"}
+              height={"20"}
+             /> 
+                <Image
+              src="/x.png"
+              alt="facebook image" 
+              width={"20"}
+              height={"20"}
+             /> 
+                <Image
+              src="/telegram.svg"
+              alt="facebook image" 
+              width={"22"}
+              height={"22"}
+             /> 
               </li>
             </ul>
           </div>

fd19d197158962db77fc4e10fe018ec63e6130e6
MohamedGlalAbdelazeem <mohamed539@gmail.com>
2024-09-29 15:00:55 +0300

    .

--- app/pages/Course-content/page.jsx
+++ app/pages/Course-content/page.jsx
@@ -1,137 +1,82 @@
 'use client';
-import { GoCheckCircleFill } from "react-icons/go";
-import { useState } from 'react';
-import { FaPlus } from "react-icons/fa";
 import ProgressBar from "@ramonak/react-progress-bar";
-import "./styles.css"
-
-
 function Page() {
-  const [currentLesson, setCurrentLesson] = useState(0); // Current lesson index
-  const [currentVideo, setCurrentVideo] = useState(0); // Current video index
-  const [expandedLesson, setExpandedLesson] = useState(null); // To track which lesson is expanded
-
-  const lessons = [
-    {
-      title: "Introduction",
-      videos: [
-        {
-          title: "Welcome to the Course",
-          videoSrc: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-        },
-      ],
-    },
-    {
-      title: "Lesson One: Cardiovascular System",
-      videos: [
-        {
-          title: "Overview of the Cardiovascular System",
-          videoSrc: "https://www.youtube.com/watch?v=3X-V8y3aRHg&ab_channel=ElWadyMusicRecords",
-        },
-        {
-          title: "Heart Anatomy",
-          videoSrc: "https://www.youtube.com/watch?v=uF96R_jjn8k&ab_channel=Doctor.EG.",
-        },
-      ],
-    },
-    {
-      title: "Lesson Two: ECG Interpretation",
-      videos: [
-        {
-          title: "Basics of ECG Interpretation",
-          videoSrc: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-        },
-        {
-          title: "Advanced ECG Techniques",
-          videoSrc: "https://www.youtube.com/watch?v=fhkyOGDbs0Q&ab_channel=%D8%A7%D9%84%D9%82%D8%B1%D8%A2%D9%86%D9%8A%D9%8F%D8%B1%D8%B4%D8%AF%D9%86%D8%A7",
-        },
-      ],
-    },
-    {
-        title: "Course Exam",
-        videos: [
-          { 
-          },
-        ],
-      },
-];
-
-  const toggleLesson = (index) => {
-    setExpandedLesson(expandedLesson === index ? null : index); // Toggle expand/collapse
-  };
-
-  const handleVideoSelect = (lessonIndex, videoIndex) => {
-    setCurrentLesson(lessonIndex);
-    setCurrentVideo(videoIndex);
-  };
-
   return (
     <>
-      <div className="h-[80px] bg-[#1f0c30f4]">
-      </div>
-      <div className="container mx-auto mt-20  px-4 my-10">
-      {/* Main Course Details */}
-      <div className="my-10 w-full flex items-center justify-between">
-        <h2 className="text-2xl font-bold mb-4 text-bgFontColor">Trial Lesson: Introduction for Diagnosis and Treatment</h2>
-        {/* Dynamic Progress */}
-        <div className="relative w-[250px] h-2 bg-[#F8EDF1] rounded-lg mb-6">
+      <div className="h-[80px] bg-[#1f0c30f4]"></div>
+      <div className="container mx-auto mt-20 px-4 my-10">
+        {/* Main Course Details */}
+        <div className="my-10 w-full flex flex-col md:flex-row items-center justify-between ">
+          <h2 className="text-xl md:text-2xl font-bold mb-4 text-bgFontColor text-center md:text-left">
+            Trial Lesson: Introduction for Diagnosis and Treatment
+          </h2>
+          {/* Dynamic Progress */}
+          <div className="relative w-full md:w-[250px] h-2 bg-[#F8EDF1] rounded-lg mb-6">
             <ProgressBar completed={80} />
+          </div>
         </div>
-      </div>
 
-      {/* Video Section */}
-      <div className="flex flex-col justify-around md:flex-row gap-8">
-        {/* Accordion for Related Lessons */}
-        <div className="w-full md:w-1/3 bg-gray-100 rounded-lg p-4">
-          <h3 className="text-xl font-semibold mb-4">Course Lessons</h3>
-          <div className="space-y-4">
-            {lessons.map((lesson, lessonIndex) => (
-              <div className="bg-white shadow-md rounded-lg transition-all" key={lessonIndex}>
-                <button
-                  type="button"
-                  className={`w-full text-sm font-semibold text-left p-6 flex items-center transition-all ${expandedLesson === lessonIndex ? 'text-blue-600' : 'text-gray-800'}`}
-                  onClick={() => toggleLesson(lessonIndex)}>
-                  <span className="mr-2 text-lg"><GoCheckCircleFill /></span>
-                  <span className="mr-4 flex items-center  justify-between w-full">
-                    {lesson.title}
-                     <FaPlus />
-                    </span>
-                </button>
+        <div className="flex flex-col md:flex-row justify-between gap-8">
+          {/* Left Section: Course Lessons and Exam */}
+          <div className="w-full md:w-1/2 ">
+            {/* Course lessons */}
+            <div className="mb-1">
+              <ul className="mt-5 p-6 w-full md:w-[440px] space-y-3 font-medium bg-gray-50">
+                {["a", "f", "e", "e", "f", "a", "f", "e", "e", "f"].map((item, index) => {
+                  return (
+                    <li key={index} className="flex items-start lg:col-span-1">
+                      <div className="flex-shrink-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                          <path fill="#fd3fc1" d="M20.79 5.72a4.1 4.1 0 0 0-2.92-1.21H8.66a4.12 4.12 0 0 0-2.92 1.22a.3.3 0 0 0-.07.08l-3.08 4.21a3.66 3.66 0 0 0-.59 2a3.74 3.74 0 0 0 .61 2l3.06 4.17l.07.09a4.1 4.1 0 0 0 2.92 1.21h9.21A4.13 4.13 0 0 0 22 15.36V8.59a4.1 4.1 0 0 0-1.21-2.87m-3.56 4.94l-3.92 3.92c-.16.161-.35.29-.56.38a1.67 1.67 0 0 1-1.34 0a1.8 1.8 0 0 1-.57-.38L8.9 12.64a1.004 1.004 0 0 1 1.42-1.42l1.76 1.77l3.74-3.74a1 1 0 1 1 1.41 1.41" />
+                        </svg>
+                      </div>
+                      <p className="ml-3 leading-5 text-lg">Lesson one</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
 
-                {/* Sub-videos inside the expanded lesson */}
-                <div className={`overflow-hidden transition-all ${expandedLesson === lessonIndex ? 'max-h-screen' : 'max-h-0'}`}>
-                  <div className="pb-6 px-6">
-                    {lesson.videos.map((video, videoIndex) => (
-                      <p
-                        key={videoIndex}
-                        className="text-sm text-gray-600 leading-relaxed cursor-pointer hover:text-blue-600"
-                        onClick={() => handleVideoSelect(lessonIndex, videoIndex)}>
-                        {video.title}
-                      </p>
-                    ))}
-                  </div>
-                </div>
+            {/* Course exam */}
+            <div className="mb-6">
+              <ul className="p-6 w-full md:w-[440px] space-y-3 font-medium bg-gray-100">
+                {["a"].map((item, index) => {
+                  return (
+                    <li key={index} className="flex items-start lg:col-span-1">
+                      <div className="flex-shrink-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                          <g fill="none" stroke="#fd3fc1" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.25">
+                            <path d="M21.25 12v3.38a3.38 3.38 0 0 1-3.38 3.382H8.661a3.38 3.38 0 0 1-2.389-.992L3.22 13.6a2.96 2.96 0 0 1 0-3.2l3.054-4.17a3.38 3.38 0 0 1 2.39-.992h9.206a3.38 3.38 0 0 1 3.38 3.382z" />
+                            <path d="m9.61 11.905l1.946 1.946a.735.735 0 0 0 1.047 0l3.922-3.921" />
+                          </g>
+                        </svg>
+                      </div>
+                      <p className="ml-3 leading-5 text-lg">Course Exam</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+
+          {/* Right Section: Main Video */}
+          <div className="w-full md:w-1/2 p-4">
+            <video controls controlsList="nodownload" width="100%" height="auto" className="rounded-2xl">
+              <source src={""} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          <div className='w-full flex justify-between my-10 gap-6'>
+            <div>
+                <button className='text-lg bg-secondary text-white p-1 rounded-lg w-28'>Enroll Now</button>
+            </div>
+              <div>
+                <button className='text-lg mx-1 border-2 border-gray-40 p-1 w-28 rounded-lg'>Prev</button>
+                <button className='text-lg mx-1 bg-bgFontColor text-white p-1 rounded-lg w-28'>Next</button>
               </div>
-            ))}
-          </div>
-        </div>
-
-        {/* Main Video on the right */}
-        <div className="flex flex-col w-full md:w-1/2 p-4">
-          <video controls  controlsList="nodownload"  width="100%" height="auto"  className="rounded-2xl">
-            <source
-              src={lessons[currentLesson].videos[currentVideo].videoSrc} 
-              type="video/mp4"
-            />
-            Your browser does not support the video tag.
-          </video>
-          <div className='w-full flex justify-end my-6 gap-6'>
-            <button className='text-lg border-2 border-bgFontColor text-bgFontColor p-1 w-28 rounded-lg '>Prev</button>
-            <button className='text-lg bg-bgFontColor text-white p-1 rounded-lg w-28'>Next</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
